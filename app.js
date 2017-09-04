11e5dcc06677cf96c29e567384c48f32b54bcf2d
Khalid Awil <khalidawil3@gmail.com>
2017-09-04 18:36:59 -0400

    Learnt about events using the v-on keyword

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ new Vue({
     data: {
         name: 'Khalid',
         job: 'Xamaali',
+        age: 22,
         website: "http://www.monster.com"
     },
     methods: {
         work: function(occupation){
             return 'Im working as a ' + occupation
+        },
+
+        add: function(val){
+            this.age += val;
+        },
+
+        subtract: function(val){
+            this.age -= val;
         }
     }
 });
