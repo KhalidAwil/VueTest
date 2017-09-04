06240bc9f6ce847cb88ad4f4cba1755ea1ca201a
Khalid Awil <khalidawil3@gmail.com>
2017-09-04 18:07:31 -0400

    Created new Vue instance and learning about directives

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 new Vue({
-    el: :''
+    el: '#vue-app',
+    data: {
+        name: 'Khalid',
+        job: 'Xamaali',
+        website: "http://www.monster.com"
+    },
+    methods: {
+        work: function(occupation){
+            return 'Im working as a ' + occupation
+        }
+    }
 });
